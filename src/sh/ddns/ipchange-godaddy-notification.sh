4f6d0f749557f210d64b32170c8d768e736dd893
Giuseppe Carlone <giuseppe.carlone@gmail.com>
2020-05-10 08:50:15 +0200

    added GoDaddy authentication problem check

--- src/sh/ddns/ipchange-godaddy-notification.sh
+++ src/sh/ddns/ipchange-godaddy-notification.sh
@@ -22,8 +22,13 @@ myIP=$( dig +short myip.opendns.com @resolver1.opendns.com )
 dnsdata=`curl -s -X GET -H "Authorization: sso-key ${gdapikey}" "https://api.godaddy.com/v1/domains/${mydomain}/records/A/${myhostname}"`
 gdIP=`echo $dnsdata | cut -d ',' -f 1 | tr -d '"' | cut -d ":" -f 2`
 
+if [ "$gdIP" == "UNABLE_TO_AUTHENTICATE" ] ; then
+  echo "`date '+%Y-%m-%d %H:%M:%S'` - Unable to Authenticate" >> $logdest
+
+  curl --silent --output /dev/null --show-error --fail -s -X POST https://api.telegram.org/bot$telegramBotToken/sendMessage -d chat_id="$telegramChatId" -d text="$HOSTNAME is unable to Authenticate to GoDaddy API"
+
 # if needed, update GoDaddy DNS IP for hostname
-if [ "$gdIP" != "$myIP" ] ; then
+elif [ "$gdIP" != "$myIP" ] ; then
   curl -s -X PUT "https://api.godaddy.com/v1/domains/${mydomain}/records/A/${myhostname}" -H "Authorization: sso-key ${gdapikey}" -H "Content-Type: application/json" -d "[{\"data\": \"${myIP}\"}]"
 
   echo "`date '+%Y-%m-%d %H:%M:%S'` - Changed GoDaddy DNS IP on ${myhostname}.${mydomain} from ${gdIP} to ${myIP}" >> $logdest
